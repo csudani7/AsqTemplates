c31abba253bcb403af38902dde53a95d7fa0438a
Chintan Sudani <csudani7@gmail.com>
2022-02-19 11:43:45 +0530

    feat: Added assets & font & dompleted remain UI work

--- assets/js/main.js
+++ assets/js/main.js
@@ -178,23 +178,17 @@
     speed: 600,
     loop: true,
     autoplay: {
-      delay: 5000,
+      delay: 3000,
       disableOnInteraction: false
     },
     slidesPerView: 'auto',
-    pagination: {
-      el: '.swiper-pagination',
-      type: 'bullets',
-      clickable: true
-    },
     breakpoints: {
       320: {
         slidesPerView: 1,
         spaceBetween: 20
       },
-
       1200: {
-        slidesPerView: 3,
+        slidesPerView: 1,
         spaceBetween: 20
       }
     }
